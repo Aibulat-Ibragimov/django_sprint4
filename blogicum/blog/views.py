d4346cf4f9cf24eef2f21e01b365a921c21c780d
Aibulat <aibulatibragimov@yandex.ru>
2023-12-09 08:56:39 +0500

    Fix bugs part 27

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -204,7 +204,10 @@ class DeleteCommentView(CommentMixin, DeleteView):
 
     def delete(self, request, *args, **kwargs):
         comment = self.get_object()
-        if self.request.user == comment.author or self.request.user.is_superuser:
+        if (
+            self.request.user == comment.author
+            or self.request.user.is_superuser
+        ):
             return super().delete(request, *args, **kwargs)
         else:
             return redirect('blog:post_detail', post_id=comment.post.id)
